c78312f1e2988225434f7f32474ca130ca985b5c
Weronciak <weronika.sumera@student.wat.edu.pl>
2023-11-18 20:10:00 +0100

    "Fugue_1" - aktualizacja S_BOX

--- Fugue_1.py
+++ Fugue_1.py
@@ -181,19 +181,20 @@ def CMIX(S):
 
 # S-BOX
 # skrzynka podstawieniowa
-def S_BOX (s_box, S):
-    # po wierszach w macierzy S
+def S_BOX (s_box, W):
+    # po wierszach w macierzy W 
     for i in range(0, 4):
-        # po kolumnach w macierzy S
-        for j in range(0, 30):
+        # po kolumnach w macierzy W
+        for j in range(0, 4):
             # column to nr kolumny w s_box
-            # column to ostatnia 4 bitowa wartosc S[i, j]
-            column = S[i, j] & 0xf
+            # column to ostatnia 4 bitowa wartosc W[i, j]
+            column = W[i, j] & 0xf
             # row to nr wiersza w s_box
-            # nastepnie przsuwamy wartosc S[i,j] o 4 bity i odczytujemy 4 ostatnie bity jako row
-            row = (S[i, j] >> 4) & 0xf
-            # przypisujemy do S[i,j] wartosc z s_box[row][column]
-            S[i, j] = s_box[row][ column]
+            # nastepnie przsuwamy wartosc W[i,j] o 4 bity i odczytujemy 4 ostatnie bity jako row
+            row = (W[i, j] >> 4) & 0xf
+            # przypisujemy do W[i,j] wartosc z s_box[row][column]
+            W[i, j] = s_box[row][ column]
+    return W
     return S
 # otwieramy plik do odczytu
 # przypisujemy zawartosc pliku do zmiennej zawartosc_pliku
